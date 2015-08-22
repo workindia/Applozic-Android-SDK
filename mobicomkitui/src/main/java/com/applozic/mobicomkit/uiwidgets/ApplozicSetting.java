63aeac51302f5556ca56e93477e63afe4d885d3a
Devashish Datt Mamgain <devashish.mamgain@gmail.com>
2015-08-22 19:17:22 +0530

    #3 ApplozicSetting for storing the UI customizations settings

---
+++ mobicomkitui/src/main/java/com/applozic/mobicomkit/uiwidgets/ApplozicSetting.java
@@ -0,0 +1,80 @@
+package com.applozic.mobicomkit.uiwidgets;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.applozic.mobicomkit.api.MobiComKitClientService;
+
+/**
+ * Created by devashish on 8/21/2015.
+ */
+public class ApplozicSetting {
+
+    private Context context;
+    public SharedPreferences sharedPreferences;
+    private static final String HANDLE_DISPLAY_NAME = "SETTING_HANDLE_DISPLAY_NAME";
+    private static final String START_NEW_FLOATING_ACTION_BUTTON_DISPLAY = "SETTING_START_NEW_FLOATING_ACTION_BUTTON_DISPLAY";
+    private static final String START_NEW_BUTTON_DISPLAY = "SETTING_START_NEW_BUTTON_DISPLAY";
+    private static final String NO_CONVERSATION_LABEL = "SETTING_NO_CONVERSATION_LABEL";
+    public static ApplozicSetting applozicSetting;
+
+    private ApplozicSetting(Context context) {
+        this.context = context;
+        sharedPreferences = context.getSharedPreferences(MobiComKitClientService.getApplicationKey(context), context.MODE_PRIVATE);
+    }
+
+    public static ApplozicSetting getInstance(Context context) {
+        if (applozicSetting == null) {
+            applozicSetting = new ApplozicSetting(context);
+        }
+
+        return applozicSetting;
+    }
+
+    public ApplozicSetting showStartNewButton() {
+        sharedPreferences.edit().putBoolean(START_NEW_BUTTON_DISPLAY, true).commit();
+        return this;
+    }
+
+    public ApplozicSetting hideStartNewButton() {
+        sharedPreferences.edit().putBoolean(START_NEW_BUTTON_DISPLAY, false).commit();
+        return this;
+    }
+
+    public boolean isStartNewButtonVisible() {
+        return sharedPreferences.getBoolean(START_NEW_BUTTON_DISPLAY, true);
+    }
+
+    public ApplozicSetting showStartNewFloatingActionButton() {
+        sharedPreferences.edit().putBoolean(START_NEW_FLOATING_ACTION_BUTTON_DISPLAY, true).commit();
+        return this;
+    }
+
+    public ApplozicSetting hideStartNewFloatingActionButton() {
+        sharedPreferences.edit().putBoolean(START_NEW_FLOATING_ACTION_BUTTON_DISPLAY, false).commit();
+        return this;
+    }
+
+    public boolean isStartNewFloatingActionButtonVisible() {
+        return sharedPreferences.getBoolean(START_NEW_BUTTON_DISPLAY, true);
+    }
+
+    public String getNoConversationLabel() {
+        return sharedPreferences.getString(NO_CONVERSATION_LABEL, context.getResources().getString(R.string.no_conversation));
+    }
+
+    public ApplozicSetting setNoConversationLabel(String label) {
+        sharedPreferences.edit().putString(NO_CONVERSATION_LABEL, label).commit();
+        return this;
+    }
+
+    public boolean isHandleDisplayName() {
+        return sharedPreferences.getBoolean(HANDLE_DISPLAY_NAME, false);
+    }
+
+    public ApplozicSetting setHandleDisplayName(String label) {
+        sharedPreferences.edit().putString(HANDLE_DISPLAY_NAME, label).commit();
+        return this;
+    }
+
+}
