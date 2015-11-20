8db47ebc4ff8dd45a4ef5900ad93b5ffc5f7b611
Devashish Datt Mamgain <devashish.mamgain@mobitexter.net>
2015-11-20 14:00:52 +0530

    #76 Setting to show/hide contact images in conversation view

--- mobicomkitui/src/main/java/com/applozic/mobicomkit/uiwidgets/ApplozicSetting.java
+++ mobicomkitui/src/main/java/com/applozic/mobicomkit/uiwidgets/ApplozicSetting.java
@@ -15,6 +15,8 @@ public class ApplozicSetting {
     private static final String START_NEW_FLOATING_ACTION_BUTTON_DISPLAY = "SETTING_START_NEW_FLOATING_ACTION_BUTTON_DISPLAY";
     private static final String START_NEW_BUTTON_DISPLAY = "SETTING_START_NEW_BUTTON_DISPLAY";
     private static final String NO_CONVERSATION_LABEL = "SETTING_NO_CONVERSATION_LABEL";
+    private static final String CONVERSATION_CONTACT_IMAGE_VISIBILITY = "CONVERSATION_CONTACT_IMAGE_VISIBILITY";
+
     public static ApplozicSetting applozicSetting;
 
     private ApplozicSetting(Context context) {
@@ -28,6 +30,20 @@ public class ApplozicSetting {
         }
 
         return applozicSetting;
+    }
+
+    public ApplozicSetting showConversationContactImage() {
+        sharedPreferences.edit().putBoolean(CONVERSATION_CONTACT_IMAGE_VISIBILITY, true).commit();
+        return this;
+    }
+
+    public ApplozicSetting hideConversationContactImage() {
+        sharedPreferences.edit().putBoolean(CONVERSATION_CONTACT_IMAGE_VISIBILITY, false).commit();
+        return this;
+    }
+
+    public boolean isConversationContactImageVisible() {
+        return sharedPreferences.getBoolean(CONVERSATION_CONTACT_IMAGE_VISIBILITY, true);
     }
 
     public ApplozicSetting showStartNewButton() {
