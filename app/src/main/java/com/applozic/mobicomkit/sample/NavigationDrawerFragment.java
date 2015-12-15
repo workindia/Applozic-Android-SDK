a268de61aa742f03ec81988e2ec844b79b78d876
Devashish Datt Mamgain <devashish.mamgain@mobitexter.net>
2015-12-15 18:47:41 +0530

    Opening chat on clicking of example action

--- app/src/main/java/com/applozic/mobicomkit/sample/NavigationDrawerFragment.java
+++ app/src/main/java/com/applozic/mobicomkit/sample/NavigationDrawerFragment.java
@@ -1,16 +1,17 @@
 package com.applozic.mobicomkit.sample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,7 +21,8 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+
+import com.applozic.mobicomkit.uiwidgets.conversation.activity.ConversationActivity;
 
 
 /**
@@ -250,7 +252,8 @@ public class NavigationDrawerFragment extends Fragment {
         }
 
         if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getActivity(), ConversationActivity.class);
+            startActivity(intent);
             return true;
         }
 
