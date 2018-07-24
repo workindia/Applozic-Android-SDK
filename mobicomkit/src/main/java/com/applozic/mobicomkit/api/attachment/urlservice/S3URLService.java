4cd40e358890d5082248950e964b36da46890482
Sunilkumarr <sunil@applozic.com>
2018-07-25 00:23:17 +0530

    Fix File download in encryption

--- mobicomkit/src/main/java/com/applozic/mobicomkit/api/attachment/urlservice/S3URLService.java
+++ mobicomkit/src/main/java/com/applozic/mobicomkit/api/attachment/urlservice/S3URLService.java
@@ -30,7 +30,7 @@ public class S3URLService implements URLService {
     @Override
     public HttpURLConnection getAttachmentConnection(Message message) throws IOException {
 
-        String response = httpRequestUtils.getResponse(mobiComKitClientService.getBaseUrl() + GET_SIGNED_URL + message.getFileMetas().getBlobKeyString(), "application/json", "application/json");
+        String response = httpRequestUtils.getResponse(mobiComKitClientService.getBaseUrl() + GET_SIGNED_URL + message.getFileMetas().getBlobKeyString(), "application/json", "application/json",true);
         if (TextUtils.isEmpty(response)) {
             return null;
         } else {
@@ -40,7 +40,7 @@ public class S3URLService implements URLService {
 
     @Override
     public String getThumbnailURL(Message message) throws IOException {
-        return httpRequestUtils.getResponse(mobiComKitClientService.getBaseUrl() + GET_SIGNED_URL + message.getFileMetas().getThumbnailBlobKey(), "application/json", "application/json");
+        return httpRequestUtils.getResponse(mobiComKitClientService.getBaseUrl() + GET_SIGNED_URL + message.getFileMetas().getThumbnailBlobKey(), "application/json", "application/json",true);
 
     }
 
