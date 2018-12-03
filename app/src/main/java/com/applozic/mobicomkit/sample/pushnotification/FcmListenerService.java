a332d08390396205755bd9d0c0789c8f466ddee9
shivampokhriyal <shivampokhriyal1997@gmail.com>
2018-12-03 18:26:54 +0530

    Update Fcm service

--- app/src/main/java/com/applozic/mobicomkit/sample/pushnotification/FcmListenerService.java
+++ app/src/main/java/com/applozic/mobicomkit/sample/pushnotification/FcmListenerService.java
@@ -3,10 +3,13 @@ package com.applozic.mobicomkit.sample.pushnotification;
 
 import android.util.Log;
 
+import com.applozic.mobicomkit.Applozic;
+import com.applozic.mobicomkit.api.account.register.RegisterUserClientService;
 import com.applozic.mobicomkit.api.account.register.RegistrationResponse;
 import com.applozic.mobicomkit.api.account.user.MobiComUserPreference;
 import com.applozic.mobicomkit.api.account.user.PushNotificationTask;
 import com.applozic.mobicomkit.api.notification.MobiComPushReceiver;
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -30,4 +33,19 @@ public class FcmListenerService extends FirebaseMessagingService {
 
     }
 
+    @Override
+    public void onNewToken(String registrationId) {
+        super.onNewToken(registrationId);
+
+        Log.i(TAG, "Found Registration Id:" + registrationId);
+        Applozic.getInstance(this).setDeviceRegistrationId(registrationId);
+        if (MobiComUserPreference.getInstance(this).isRegistered()) {
+            try {
+                RegistrationResponse registrationResponse = new RegisterUserClientService(this).updatePushNotificationId(registrationId);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
