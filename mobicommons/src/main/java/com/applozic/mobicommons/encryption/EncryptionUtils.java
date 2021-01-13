c6a8c5869fb5392ab654e6c37476c7544a7cf936
Sunilkumarr <sunil@applozic.com>
2021-01-13 15:28:30 +0530

    Release v5.96.0.1

--- mobicommons/src/main/java/com/applozic/mobicommons/encryption/EncryptionUtils.java
+++ mobicommons/src/main/java/com/applozic/mobicommons/encryption/EncryptionUtils.java
@@ -3,10 +3,8 @@ package com.applozic.mobicommons.encryption;
 import android.text.TextUtils;
 import android.util.Base64;
 
-import java.security.Key;
-import java.util.Arrays;
-
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 
@@ -16,50 +14,35 @@ import javax.crypto.spec.SecretKeySpec;
 public class EncryptionUtils {
 
     private static final String TAG = "EncryptionUtils";
-    private static final String ALGORITHM = "AES/ECB/NoPadding";
+    private static final String ALGORITHM = "AES/CBC/PKCS5PADDING";
     private static final String ALGORITHM_AES = "AES";
 
     // Performs Encryption
-    public static String encrypt(String ketString, String plainText) throws Exception {
-        // generate key
-        if(TextUtils.isEmpty(plainText)){
+    public static String encrypt(String keyString, String plainText) throws Exception {
+        if (TextUtils.isEmpty(plainText)) {
             return null;
         }
-        Key key =  generateKey(ketString);
-        Cipher chiper = Cipher.getInstance(ALGORITHM);
-        chiper.init(Cipher.ENCRYPT_MODE, key);
-        byte[] w = plainText.getBytes();
-        int bufferLength = 3072;
-        int i = 0;
-        byte[] buf = new byte[bufferLength];
-        StringBuffer stringBuffer = new StringBuffer();
-        while (i < w.length) {
-            int size = Math.min(bufferLength, w.length - i);
-            Arrays.fill(buf, (byte) 0);
-            System.arraycopy(w, i, buf, 0, size);
-            buf = chiper.doFinal(buf);
-            stringBuffer.append(Base64.encodeToString(buf,Base64.DEFAULT));
-            i += size;
-        }
-        return stringBuffer.toString();
+        SecretKeySpec secretKeySpec = generateKey(keyString);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec,
+                new IvParameterSpec(new byte[16]));
+        byte[] encrypted = cipher.doFinal(plainText.getBytes());
+        return Base64.encodeToString(encrypted, Base64.DEFAULT);
     }
 
     // Performs decryption
-    public static String decrypt(String ketString, String encryptedText) throws Exception {
-        // generate key
-        Key key =  generateKey(ketString);
-        Cipher chiper= Cipher.getInstance(ALGORITHM);
-        chiper.init(Cipher.DECRYPT_MODE, key);
-        byte[] decodedValue = Base64.decode(encryptedText,Base64.DEFAULT);
-        byte[] decValue = chiper.doFinal(decodedValue);
-        String decryptedValue = new String(decValue);
-        return TextUtils.isEmpty(decryptedValue)?null:decryptedValue.trim();
+    public static String decrypt(String keyString, String encryptedText) throws Exception {
+        SecretKeySpec secretKeySpec = generateKey(keyString);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec,
+                new IvParameterSpec(new byte[16]));
+        byte[] decodedValue = Base64.decode(encryptedText, Base64.DEFAULT);
+        byte[] original = cipher.doFinal(decodedValue);
+        return new String(original);
     }
 
     //generateKey() is used to generate a secret key for AES algorithm
-    private static Key generateKey(String ketString) throws Exception {
-        Key key = new SecretKeySpec(ketString.getBytes(), ALGORITHM_AES);
-        return key;
+    private static SecretKeySpec generateKey(String keyString) throws Exception {
+        return new SecretKeySpec(keyString.getBytes("UTF-8"), ALGORITHM_AES);
     }
-
 }
