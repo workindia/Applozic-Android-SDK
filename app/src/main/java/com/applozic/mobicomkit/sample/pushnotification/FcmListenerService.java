c2def7ffaee207cf32e6f674c18e36f73c04bd80
shubham-applozic <shubham@applozic.com>
2021-06-07 07:32:18 +0530

    CMK-162: remove unused imports

--- app/src/main/java/com/applozic/mobicomkit/sample/pushnotification/FcmListenerService.java
+++ app/src/main/java/com/applozic/mobicomkit/sample/pushnotification/FcmListenerService.java
@@ -7,9 +7,6 @@ import com.applozic.mobicomkit.Applozic;
 import com.applozic.mobicomkit.api.account.register.RegisterUserClientService;
 import com.applozic.mobicomkit.api.account.register.RegistrationResponse;
 import com.applozic.mobicomkit.api.account.user.MobiComUserPreference;
-import com.applozic.mobicomkit.api.account.user.PushNotificationTask;
-import com.applozic.mobicomkit.api.notification.MobiComPushReceiver;
-import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
